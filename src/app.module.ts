586b3c32aa02dd53fb070681661b4ce1eb525f05
proav2323 <108835201+proav2323@users.noreply.github.com>
2024-02-11 14:08:29 -0600

    coding

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaServiceService } from './prisma-service/prisma-service.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaServiceService],
 })
 export class AppModule {}
