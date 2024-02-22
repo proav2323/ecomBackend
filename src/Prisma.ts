781ad2415abdd41a7f4959458690d8cc9f671a85
proav2323 <108835201+proav2323@users.noreply.github.com>
2024-02-21 20:18:05 -0600

    coding and this is new

--- src/Prisma.ts
+++
@@ -1,7 +0,0 @@
-// prisma.types.ts
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export type Prisma = typeof prisma;
-export const prismaClient = prisma;
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,9 +5,4 @@ import { prismaClient } from './Prisma';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('/')
-  getPrismaTypes() {
-    return prismaClient;
-  }
 }
