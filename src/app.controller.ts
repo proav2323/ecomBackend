fdfaeb973d35a31b4c31b9f4da2f9d089456c422
proav2323 <108835201+proav2323@users.noreply.github.com>
2024-02-21 20:21:49 -0600

    coding and this is new

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { prismaClient } from './Prisma';
 
 @Controller()
 export class AppController {
